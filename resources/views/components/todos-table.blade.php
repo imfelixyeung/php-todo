98c2a68905acc49a85fcda6413418b9a323c900c
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-05-21 23:22:20 +0100

    refactor todos table to component and add zero length case

---
+++ resources/views/components/todos-table.blade.php
@@ -0,0 +1,31 @@
+@props(['todos' => []])
+
+<div class="border rounded-lg p-2 mt-6">
+    @if (count($todos) == 0)
+        <div>
+            <p>You have no todos yet, create one to get started!</p>
+        </div>
+    @else
+        <table class="w-full">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th class="text-left">Name</th>
+                    <th class="text-left">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($todos as $todo)
+                    <tr>
+                        <td>
+                            <input type="checkbox" @if ($todo['completed']) checked @endif />
+                        </td>
+                        <td class="{{ $todo['completed'] ? 'line-through' : '' }}">{{ $todo['name'] }}</td>
+                        <td>{{ $todo['completed'] ? 'Done' : 'Todo' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    @endif
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,26 +1,5 @@
 <x-layout>
     <h1 class="text-3xl font-bold">Todos</h1>
 
-    <div class="border rounded-lg p-2 mt-6">
-        <table class="w-full">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th class="text-left">Name</th>
-                    <th class="text-left">Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($todos as $todo)
-                    <tr>
-                        <td>
-                            <input type="checkbox" @if ($todo['completed']) checked @endif />
-                        </td>
-                        <td class="{{ $todo['completed'] ? 'line-through' : '' }}">{{ $todo['name'] }}</td>
-                        <td>{{ $todo['completed'] ? 'Done' : 'Todo' }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <x-todos-table :todos="$todos" />
 </x-layout>
