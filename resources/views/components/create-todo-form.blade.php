3ba5f0459e14e678a23eb36083a2876c6dfb16fe
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-05-22 00:16:04 +0100

    add create new todo form ui

---
+++ resources/views/components/create-todo-form.blade.php
@@ -0,0 +1,7 @@
+<form action="">
+    <div class="grid grid-cols-[auto_1fr_auto] max-w-96 w-full border rounded-lg">
+        <label for="newTodo" class="block px-2 py-1 border-r">New Todo</label>
+        <input name="newTodo" id="newTodo" placeholder="Bake a cake.." class="px-2 py-1" />
+        <button type="submit" class="px-2 py-1 border-l cursor-pointer">Create</button>
+    </div>
+</form>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
     <h1 class="text-3xl font-bold">Todos</h1>
 
+    <x-create-todo-form />
     <x-todos-table :todos="$todos" />
 </x-layout>
