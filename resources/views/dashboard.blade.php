c8a588fdda53d4611fc754e252fa426ede389500
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-05-21 23:13:32 +0100

    add dashboard page and feed dummy data via backend

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+    <h1 class="text-3xl font-bold">Todos</h1>
+
+    <div class="border rounded-lg p-2 mt-6">
+        <table class="w-full">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th class="text-left">Name</th>
+                    <th class="text-left">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($todos as $todo)
+                    <tr>
+                        <td>
+                            <input type="checkbox" @if ($todo['completed']) checked @endif />
+                        </td>
+                        <td>{{ $todo['name'] }}</td>
+                        <td>{{ $todo['completed'] ? 'Done' : 'Todo' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard', [
+        "todos" => [
+            [
+                "name" => "Learn PHP",
+                "completed" => true
+            ],
+            [
+                "name" => "Learn Laravel",
+                "completed" => true
+            ],
+            [
+                "name" => "Learn MySQL",
+                "completed" => false
+            ],
+        ]
+    ]);
+});
