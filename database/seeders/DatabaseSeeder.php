51cace64679d957a684c06e4b9ab1a825d6ba30d
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-05-22 16:40:32 +0100

    add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Todo;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Felix',
+            'email' => 'me@felixyeung.com',
         ]);
+
+        Todo::create([
+            "name" => "Learn Laravel",
+            "completed" => true,
+        ]);
+        Todo::create([
+            "name" => "Learn PHP",
+            "completed" => true,
+        ]);
+        Todo::create([
+            "name" => "Learn MySQL",
+            "completed" => false,
+        ]);
+
+        Todo::factory(50)->create();
     }
 }
