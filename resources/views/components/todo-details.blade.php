3d4348284c341c0e584f061eab662092a574454c
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-05-23 15:44:17 +0100

    remove unused form fields

--- resources/views/components/todo-details.blade.php
+++ resources/views/components/todo-details.blade.php
@@ -19,7 +19,6 @@
             <form method="POST" action="/dashboard/todos/{{ $todo['id'] }}/update">
                 @csrf
                 <x-form-page page="{{ $page }}" />
-                <input name="todoId" value="{{ $todo['id'] }}" hidden />
                 <input name="completed" type="checkbox" @if (!$todo['completed']) checked @endif hidden />
                 <button type="submit" class="px-3 py-1 border rounded-sm inline-block">Mark as <x-todo-status-label
                         completed="{{ !$todo['completed'] }}" /></button>
@@ -27,7 +26,6 @@
             <form method="POST" action="/dashboard/todos/{{ $todo['id'] }}/delete">
                 @csrf
                 <x-form-page page="{{ $page }}" />
-                <input name="todoId" value="{{ $todo['id'] }}" hidden />
                 <button type="submit" class="px-3 py-1 border rounded-sm inline-block">Delete</button>
             </form>
         </div>
