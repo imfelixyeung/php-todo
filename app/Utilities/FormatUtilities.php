f6df750ef723ece7e5e00ec70c5c74988f119563
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-05-24 00:08:14 +0100

    specify formatter timezone to be Europe/London

--- app/Utilities/FormatUtilities.php
+++ app/Utilities/FormatUtilities.php
@@ -3,16 +3,19 @@
 namespace App\Utilities;
 
 use DateTime;
+use DateTimeZone;
 
 class FormatUtilities
 {
   public static function formatDate(DateTime $date)
   {
+    $date->setTimezone(new DateTimeZone('Europe/London'));
     return $date->format('d M Y');
   }
 
   public static function formatLongDate(DateTime $date)
   {
+    $date->setTimezone(new DateTimeZone('Europe/London'));
     return $date->format('d M Y H:i');
   }
 }
