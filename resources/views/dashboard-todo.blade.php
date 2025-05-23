33f784076fbf0b330b9e30266951b470f7ed491c
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-05-23 15:55:34 +0100

    make dashboard mobile responsive

--- resources/views/dashboard-todo.blade.php
+++ resources/views/dashboard-todo.blade.php
@@ -1,14 +1,15 @@
 <x-layout>
-    <div class="grid grid-cols-[auto_20rem] h-full">
-        <div class="p-6">
+    <div class="grid grid-cols-1 grid-rows-[auto_auto] md:grid-cols-[auto_20rem] md:grid-rows-1 h-full">
+        <div
+            class="px-6 pb-6 pt-12 md:border-l border-b md:border-b-0 relative grid grid-rows-[1fr] md:col-start-2 md:col-span-1 md:row-start-1">
+            <a href="/dashboard?page={{ $todos->currentPage() }}" class="absolute top-3 left-6">Close</a>
+            <x-todo-details :todo="$todo" page="{{ $todos->currentPage() }}" />
+        </div>
+        <div class="p-6 md:col-start-1 md:col-span-1 md:row-start-1">
             <h1 class="text-3xl font-bold">Todos</h1>
 
             <x-create-todo-form />
             <x-todos-table :todos="$todos" :selectedTodo="$todo" page="{{ $todos->currentPage() }}" />
-        </div>
-        <div class="px-6 pb-6 pt-12 border-l relative grid grid-rows-[1fr]">
-            <a href="/dashboard?page={{ $todos->currentPage() }}" class="absolute top-3 left-6">Close</a>
-            <x-todo-details :todo="$todo" page="{{ $todos->currentPage() }}" />
         </div>
     </div>
 </x-layout>
