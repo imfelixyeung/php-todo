e36ff7925fdde01dbb59678a103e283ac9634fee
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-05-23 21:13:50 +0100

    add header auth ui

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,17 @@
 <header class="border-b bg-gray-100 px-6">
-    <div class="flex items-center min-h-14">
+    <div class="flex items-center min-h-14 justify-between">
         <div class="text-xl font-medium">
             Php Todo
+        </div>
+        <div class="flex gap-3">
+            @guest()
+                <a href="/login" class="block">Login</a>
+                <a href="/register" class="block">Register</a>
+            @endguest
+            @auth
+                Hello, User!
+                {{-- todo: implement user's name --}}
+            @endauth
         </div>
     </div>
 </header>
