d1e1d531620901657977f6914edb3d6f11a50fa1
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-05-24 22:04:31 +0100

    build: use vite tailwindcss over cdn playground

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Todo</title>
 
-    <x-tailwindcss />
+    @vite('resources/css/app.css')
 </head>
 
 <body>
